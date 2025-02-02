c1f2a86a9f158fe0da8eb648806fcf754280f5f8
Andrei Stolarov <andrey.stolyarov.1962@mail.ru>
2025-02-02 23:34:17 +0300

    добавлен фунционал

--- script.js
+++ script.js
@@ -1,45 +1,33 @@
-const burger = document.getElementById("burger");               /* создаем элемент по id    */
+let burger = document.querySelector('#burger')  
+let nav = document.querySelector('#nav')   
+burger.addEventListener('click', () => {  
+  nav.classList.toggle('navigation__active');
+  burger.classList.toggle('header__burger_active');    
+})
 
-/* по клику - addEventListener("click") - меняет класс - classList.toggle("header__burger_active") -  */
-burger.addEventListener("click", burger.classList.toggle("header__burger_active"));
+let btnForm = document.querySelector('#submit')  
+let success = document.querySelector('#success') 
+let error = document.querySelector('#error')  
+let inputName = document.querySelector('#name')
+let inputEmail = document.querySelector('#email')
+let textareaMessage = document.querySelector('#message') 
 
-const nav = document.getElementById("nav");
-nav.addEventListener("click", nav.classList.toggle("navigation__active"));
+btnForm.addEventListener('click', (event) => {
+  event.preventDefault()
 
-const submit = document.getElementById("submit");
-/* по клику сбрасываем отправку формы - preventDefault */
-submit.addEventListener("click", submit.preventDefault());
-
-const error = document.getElementById("error");
-error.addEventListener("/*click*/", error.classList.add("form__fields-error_active"));
-error.addEventListener("click", error.preventDefault());
-
-const success = document.getElementById("success");
-success.addEventListener("click", success.preventDefault());
-
-const name = document.getElementById("name");
-name.addEventListener("click", name.preventDefault());
-name.setAttribute('required', '');
-
-const message = document.getElementById("message");
-message.addEventListener("click", message.preventDefault());
-message.setAttribute('required', '');
-
-const email = document.getElementById("email");
-email.addEventListener("click", email.preventDefault());
-email.setAttribute('required', '');
-
-if (name.trim() === " " && email.trim() === " " && message.trim() === " ") {
-  error.classList.add("form__fields-error_active");
-}
-else {
-  success.classList.add("wrapper-success_active");/* устанавливаем класс*/
-  error.classList.remove("form__fields-error_active");/*удаляем класс*/
-  document.getElementById('name').value = '';/* значение по id обнуляем */
-  document.getElementById('email').value = '';
-  document.getElementById('message').value = '';
-}
-function greet() {
-  success.classList.remove("wrapper-success_active");
-}
-const timeoutID = setTimeout(greet, 3000);
+  if (inputName.value === "" || inputEmail.value === "" || textareaMessage.value === "") {
+    error.classList.add('form__fields-error_active')  
+    setTimeout(() => {
+      error.classList.remove('form__fields-error_active')
+    }, 1000)                                                      
+  } else {
+    success.classList.add('wrapper-success_active')      
+    error.classList.remove('form__fields-error_active')   /*удаляем класс*/
+    inputName.value = ''                            /* значение по id обнуляем */
+    inputEmail.value = ''       
+    textareaMessage.value = ''
+    setTimeout(() => {
+      success.classList.remove('wrapper-success_active');
+    },3000)
+  }
+})
